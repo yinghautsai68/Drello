72443e64b5d0c721c2d01ed1e62b7d710011b020
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-26 18:50:59 +0800

    feat(backend): 建立資料庫連線與環境變數設定

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import { db } from './config/db';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const testDB = async () => {
+    try {
+        const result = await db.query('SELECT 1');
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+testDB();
 app.listen(5000, () => {
     console.log('server running');
 });
