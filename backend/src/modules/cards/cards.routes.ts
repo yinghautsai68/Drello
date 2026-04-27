2785c9d9c683c76781eb1314f606ea0b9f190c9a
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-27 20:58:54 +0800

    feat(backend/cards): 路由新增驗證中介層

--- backend/src/modules/cards/cards.routes.ts
+++ backend/src/modules/cards/cards.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { handleCreateCard, handleDeleteCard, handleGetCards, handleUpdateCard } from './cards.controller';
+import { authenticate } from '../../middlewares/authenticate';
 
 const cardsRouter = express.Router();
 
-cardsRouter.post('/', handleCreateCard);
-cardsRouter.get('/', handleGetCards);
-cardsRouter.patch('/:id', handleUpdateCard);
-cardsRouter.delete('/:id', handleDeleteCard);
+cardsRouter.post('/', authenticate, handleCreateCard);
+cardsRouter.get('/', authenticate, handleGetCards);
+cardsRouter.patch('/:id', authenticate, handleUpdateCard);
+cardsRouter.delete('/:id', authenticate, handleDeleteCard);
 
 export default cardsRouter;
